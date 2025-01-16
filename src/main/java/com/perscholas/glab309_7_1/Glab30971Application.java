6a9d0042b6d1b7e42df3650a143dab9aa4a0cf36
Twincasper <justinwatson651@gmail.com>
2025-01-16 12:34:20 -0500

    Reestablished spring boot rest dependency

--- src/main/java/com/perscholas/glab309_7_1/Glab30971Application.java
+++ src/main/java/com/perscholas/glab309_7_1/Glab30971Application.java
@@ -2,8 +2,12 @@ package com.perscholas.glab309_7_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.perscholas.glab309_7_1.repository")
+@EntityScan("com.perscholas.glab309_7_1.model")
 public class Glab30971Application {
 
     public static void main(String[] args) {
